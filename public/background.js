07470e6fe1bec3adb91ef4165fd09c0ee80a8361
Shoumyo <shoumyoarchit@gmail.com>
2018-09-18 20:19:32 -0400

    Renaming files to camel case for consistency with React source code

--- public/background.js
+++ public/background.js
@@ -1,29 +1,8 @@
 let popupWindowManager = new PopupWindowManager();
-
-function inject_all_scripts(tab_id, script_list) {
-    chrome.tabs.executeScript(tab_id, {file: script_list[0]}, function() {
-        script_list.shift();
-        if(script_list.length > 0) {
-            inject_all_scripts(tab_id, script_list);
-        }
-    });
-}
-
-function requestSource() {
-    if(!location.href.includes('chrome://')) {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            inject_all_scripts(tabs[0].id, [
-                "libs/jquery.min.js", 
-                "helper_scripts/parsers.js", 
-                "helper_scripts/parse_document.js"
-            ]);
-        });
-    }
-}
+let pageSourceScraper = new PageSourceScraper();
 
 chrome.tabs.onUpdated.addListener(function(activeInfo) {
-    console.log('updated');
-    requestSource();
+    pageSourceScraper.requestSource();
 });
 
 function sendCollectedData(payload) {
@@ -53,19 +32,10 @@ function sendCollectedData(payload) {
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (request.action == "get_source") {
         if(chrome.runtime.lastError || request.error) {
-            console.log('not got source')
             console.log(request.error);
         } else {
-            console.log('got source')
-            // console.log(request.data);
-        
-            sendCollectedData(request.data)
+            console.log('got source');
+            sendCollectedData(request.data);
         }
-    } 
-    else if(request.action == "get_availability") {
-        console.log('got availability')
-        console.log(request.data);
-
-        sendTestingData(request.data);
-    } 
+    }
 });
