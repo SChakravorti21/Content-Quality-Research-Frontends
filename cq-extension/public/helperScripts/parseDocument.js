2cabc57e3bd40c2dd8a282790147feff8167f46d
Shoumyo <shoumyoarchit@gmail.com>
2018-11-23 00:51:33 -0500

    Updated Reddit parser to make async GET requests for user karma

--- cq-extension/public/helperScripts/parseDocument.js
+++ cq-extension/public/helperScripts/parseDocument.js
@@ -1,70 +1,74 @@
-function initializeParser(href) {
-    if(href.includes('wiki')) {
-        return new WikiParser();
-    } else if(href.includes('brainly')) {
-        return new BrainlyParser();
-    } else if(site_href.includes('answers.com')) {
-        return new AnswersParser();
-    } else if(site_href.includes('reddit')) {
-        return new RedditParser();
-    } else if(site_href.includes('answerbag')) {
-        return new AnswerbagParser();
-    } else if(site_href.includes('stackexchange') || site_href.includes('stackoverflow')) {
-        return new StackExchangeParser();
-    } else {
-        return new Parser();
-    }
-}
+(function() {
 
-let hostname = location.hostname;
-let site_href = location.href;
-let source = hostname.substr(hostname.lastIndexOf(".") + 1);
+    if(window.workingOnParsing)
+        return;
 
-// Initialize the page parses based on the respective page
-// and collect the data that is common for all pages.
-var parser = initializeParser(site_href);
-var data = {
-    //full: parser.DOMToString(),
-    href: site_href,
-    domain: hostname,
-    source: source,
-    time: Date.now()
-};
+    window.workingOnParsing = true;
 
-try {
-
-    if(site_href.includes('wiki')) {
-        data.wiki_data = parser.getParsedWikiPage(['.mw-parser-output', '#mw-content-text']);
-    } else if(site_href.includes('brainly')) {
-        data.brainly_data = parser.getParsedBrainlyPage();
-        new BrainlyModifier().modifyPageSource();
-    } else if(site_href.includes('answers')) {
-        data.answers_data = parser.getParsedAnswersPage();
-    } else if(site_href.includes('reddit')) {
-        data.reddit_data = parser.getParsedRedditPage();
-        console.log(data.reddit_data);
-    } else if(site_href.includes('answerbag')) {
-        data.answerbag_data = parser.getParsedAnswerbagPage();
-    } else if(site_href.includes('stackexchange') || site_href.includes('stackoverflow')) {
-        data.stackexchange_data = parser.getParsedStackExchangePage();
+    function initializeParser(href) {
+        if(href.includes('wiki')) {
+            return new WikiParser();
+        } else if(href.includes('brainly')) {
+            return new BrainlyParser();
+        } else if(site_href.includes('answers.com')) {
+            return new AnswersParser();
+        } else if(site_href.includes('reddit')) {
+            return new RedditParser();
+        } else if(site_href.includes('answerbag')) {
+            return new AnswerbagParser();
+        } else if(site_href.includes('stackexchange') || site_href.includes('stackoverflow')) {
+            return new StackExchangeParser();
+        }
     }
 
-    // Only for printing the JSON so that it can be emailed for reference
-    // console.log(JSON.stringify(data))
+    function resolvePageSource(data) {
+        chrome.runtime.sendMessage({
+            action: "get_source",
+            data: data
+        });
+    }
 
-    chrome.runtime.sendMessage({
-        action: "get_source",
-        data: data
-    });
+    let hostname = location.hostname;
+    let site_href = location.href;
+    let source = hostname.substr(hostname.lastIndexOf(".") + 1);
 
-} catch(error) {
-    chrome.runtime.sendMessage({
-        action: "get_source",
-        'error': error.message
-    });
+    // Initialize the page parses based on the respective page
+    // and collect the data that is common for all pages.
+    let parser = initializeParser(site_href);
+    let data = {
+        //full: parser.DOMToString(),
+        href: site_href,
+        domain: hostname,
+        source: source,
+        time: Date.now()
+    };
 
-    chrome.runtime.sendMessage({
-        action: "get_source",
-        data: data
-    });
-}
+    try {
+
+        let resolveImmediately = true;
+        if(site_href.includes('wiki')) {
+            data.wiki_data = parser.getParsedWikiPage(['.mw-parser-output', '#mw-content-text']);
+        } else if(site_href.includes('brainly')) {
+            data.brainly_data = parser.getParsedBrainlyPage();
+            new BrainlyModifier().modifyPageSource();
+        } else if(site_href.includes('answers')) {
+            data.answers_data = parser.getParsedAnswersPage();
+        } else if(site_href.includes('reddit')) {
+            resolveImmediately = false;
+            data.reddit_data = parser.getParsedRedditPage(resolvePageSource);
+        } else if(site_href.includes('answerbag')) {
+            data.answerbag_data = parser.getParsedAnswerbagPage();
+        } else if(site_href.includes('stackexchange') || site_href.includes('stackoverflow')) {
+            data.stackexchange_data = parser.getParsedStackExchangePage();
+        }
+        
+        if(resolveImmediately)
+            resolvePageSource(data);
+
+    } catch(error) {
+        chrome.runtime.sendMessage({
+            action: "get_source",
+            'error': error.message
+        });
+    }
+})();
--- cq-extension/public/helperScripts/parsers/RedditParser.js
+++ cq-extension/public/helperScripts/parsers/RedditParser.js
@@ -4,10 +4,7 @@
     window.RedditParser = class RedditParser extends QAParser {
         constructor() {
             super('a.title', 'div.md-container', 'div.score.unvoted');
-            this.answer_selectors = [
-                '.commentarea .comment[itemprop="acceptedAnswer"] > .entry.unvoted',
-                '.commentarea .comment[itemprop="suggestedAnswer"] > .entry.unvoted'
-            ]
+            this.answer_selector = '.commentarea > .sitetable > .comment > .entry.unvoted';
         }
 
         getTimesGilded(answer_node, gild_path) {
@@ -27,32 +24,57 @@
             return Number.parseInt(karma_elem.getAttribute('title'));
         }
 
+        fetchUserKarma(username, errorCallback) {
+            return fetch(`https://old.reddit.com/user/${username}/about.json`)
+                .then(response => response.json())
+                .then(user => user['data']['link_karma'] + user['data']['comment_karma'])
+                .catch(status, err => errorCallback(err));
+        }
+
         getAnswers() {
-            let all_answers = [];
-            this.answer_selectors.forEach(selector => {
-                document.querySelectorAll(selector).forEach(answer_node => {
+            return new Promise((resolve, reject) => {
+
+                let get_requests = [];
+                let all_answers = [];
+
+                document.querySelectorAll(this.answer_selector).forEach(answer_node => {
                     const username_elem = answer_node.querySelector('.tagline .author');
                     const body_elem = answer_node.querySelector('.usertext-body .md');
-                    
+                    if(!username_elem || !body_elem) return;
+
+                    const username = username_elem.innerText;
+                    get_requests.push(this.fetchUserKarma(username, reject));
+
                     all_answers.push({
-                        username: username_elem.innerText,
+                        username: username,
                         answer: body_elem.innerText,
                         upvotes: this.getKarma(answer_node),
                         silver: this.getTimesGilded(answer_node, 'span.gilded-gid1-icon'),
                         gold: this.getTimesGilded(answer_node, 'span.gilded-gid2-icon')
-                    })
-                })
-            })
+                    });
 
-            return all_answers;
+                });
+
+                Promise.all(get_requests)
+                    .then(results => {
+                        all_answers.forEach((answer, index) => {
+                            answer['user_karma'] = results[index];
+                        });
+                    })
+                    .then(() => resolve(all_answers))
+                    .catch(error => reject(error));
+            });
         }
 
-        getParsedRedditPage() {
-            return {
-                title: this.getQuestion(),
-                body: this.getBody(),
-                all_answers: this.getAnswers()
-            }
+        getParsedRedditPage(resolvePageSourceCallback) {
+            this.getAnswers()
+                .then(all_answers => {
+                    resolvePageSourceCallback({
+                        title: this.getQuestion(),
+                        body: this.getBody(),
+                        all_answers: all_answers
+                    });
+                });
         }
 
     }
